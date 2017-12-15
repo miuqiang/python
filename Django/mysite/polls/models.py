cba2bcc9af6cb6ad51f03157e64c043643206ecf
miao.qiang <miao.qiang04894@westone.com.cn>
2017-12-15 10:50:05 +0800

    新增数据库

--- Django/mysite/polls/models.py
+++ Django/mysite/polls/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class userInfo(models.Model):
+    username=models.CharField(max_length=50)
+    email=models.CharField(max_length=50)
+    tel=models.CharField(max_length=50)
--- Django/mysite/polls/views.py
+++ Django/mysite/polls/views.py
@@ -1,19 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from . import models
+
 # Create your views here.
 def index(request):
     return HttpResponse('<h1>h1</h1>')
 
-USER_LIST = []
 def userInfo(request):
     if request.method=='POST':
-        username=request.POST.get('username',None)
-        email=request.POST.get('email',None)
-        tel=request.POST.get('tel',None)
+        u=request.POST.get('username',None)
+        e=request.POST.get('email',None)
+        t=request.POST.get('tel',None)
 
-        if username and email and tel:
-            USER_LIST.append({'username':username,'email':email,'tel':tel})
+        models.userInfo.objects.create(username=u,email=e,tel=t)
+        
+    USER_LIST = models.userInfo.objects.all()
             
     return render(request,'userInfo.html',{'USER_LIST':USER_LIST})
-
